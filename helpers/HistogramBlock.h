10e7784dd7fb09465c188fd8140e9a875b1e5ffc
Miles Wu <mileswu@uchicago.edu>
2013-07-10 09:32:22 -0500

    Remove obsolete helpers

--- helpers/HistogramBlock.h
+++
@@ -1,9 +0,0 @@
-#include "AnalysisBase.h"
-#include "TH1D.h"
-#include "TH2D.h"
-#include "TROOT.h"
-#include <vector>
-
-void InitializeRegionBlock(AnalysisBase *analysis, TString blockname);
-void FillRegionBlock(AnalysisBase *analysis, TString blockname);
-
